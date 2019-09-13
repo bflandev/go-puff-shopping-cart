519025da9e1b21ad6126878c822350853167a73d
Brian Flanagan <bflan@Brians-MBP.fios-router.home>
2019-09-13 18:23:27 -0400

    add ui class to app.js and bootstrap cart button

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
+const cartBtn = document.querySelector('.cart-btn');
+const cartCloseBtn = document.querySelector('.close-cart-btn');
+const cartDom = document.querySelector('.cart');
+const cartOverlayDom = document.querySelector('.cart-overlay');
 class CartService {
     async getCart() {
         try {
@@ -9,9 +13,31 @@ class CartService {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+class UI {
+    bootstrap() {
+        cartBtn.addEventListener('click', () => {
+            this.openCart();
+        })
+        cartCloseBtn.addEventListener('click', () => {
+            this.closeCart();
+        })
+    }
 
+    openCart() {
+        cartDom.classList.add('cart-open');
+        cartOverlayDom.classList.add('show-overlay');
+    }
+    closeCart() {
+        cartDom.classList.remove('cart-open');
+        cartOverlayDom.classList.remove('show-overlay');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     const cartService = new CartService();
+    const ui = new UI();
+
+    ui.bootstrap();
     cartService.getCart().then(cart => {
         console.log(cart);
     });
